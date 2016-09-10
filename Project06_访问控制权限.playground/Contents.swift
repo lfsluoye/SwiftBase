084b7eda87e4aa43baede2d0e5ef6cb49810b047
lfs736012 <lfs736012@163.com>
2016-09-10 14:53:12 +0800

    访问控制权限

---
+++ Project06_访问控制权限.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let app = App(name: "My First App")
+app.show()
+app.swichMode()
+app.show()
---
+++ Project06_访问控制权限.playground/Sources/UI.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+enum Theme {
+    case DayMode
+    case NightMode
+}
+class UI{
+    private var fontColor: UIColor!
+    private var backgroundColor: UIColor!
+    var themeMode: Theme = .DayMode{
+        didSet{
+            self.changeTheme(themeMode)
+        }
+    }
+    
+    init(){
+        self.themeMode = .DayMode
+        self.changeTheme(self.themeMode)
+    }
+    
+    init(themeMode: Theme){
+        self.themeMode = themeMode
+        self.changeTheme(themeMode)
+    }
+    private func changeTheme(themeMode: Theme){
+        switch themeMode {
+        case .DayMode:
+            fontColor = UIColor.blackColor()
+            backgroundColor = UIColor.whiteColor()
+        case .NightMode:
+            fontColor = UIColor.whiteColor()
+            backgroundColor = UIColor.blackColor()
+            
+        }
+    }
+    func show() {
+        print("The font color is \(fontColor == UIColor.whiteColor() ? " WHITE" : " BLACK")")
+        print("The background color is \(backgroundColor == UIColor.whiteColor() ? "WHITE" : "BLACK")")
+    }
+}
+
+
+
