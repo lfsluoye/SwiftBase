e43c90a57d986c2ae89a36135ba7d334992654d6
lfs736012 <lfs736012@163.com>
2016-09-10 16:45:16 +0800

    单例初探

---
+++ Project07_单例.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let gameManager = GameManager.defaultGameManager
+gameManager.addScore()
+gameManager.score
+
+
+let gm = GameManager.defaultGameManager
+gm.addScore()
+gm.score
